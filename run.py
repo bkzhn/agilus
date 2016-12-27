039cd10b4050b9b7c8cf35e96ab81152921641a9
bkzhn <absh.bktl@gmail.com>
2016-12-28 00:44:23 +0600

    initialize SQLAlchemy object for app in run.py

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
