a7b73ac057e5e74c5751c498dbe853bf2ac1b5e7
bkzhn <absh.bktl@gmail.com>
2016-12-29 00:12:58 +0600

    declare User() #model

--- models.py
+++ models.py
@@ -1,5 +1,17 @@
 from run import db
 
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = db.Column(db.String(100), nullable=False)
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<User %r>' % self.name
+
+
 class TicketType(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
