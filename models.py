4d2e1b828c8f13bb6055a9aea47c15af2910ea1b
bkzhn <absh.bktl@gmail.com>
2016-12-29 00:19:44 +0600

    declare TicketStatus() #model

--- models.py
+++ models.py
@@ -21,3 +21,14 @@ class TicketType(db.Model):
 
     def __repr__(self):
         return '<TicketType %r>' % self.name
+
+
+class TicketStatus(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = db.Column(db.String(100), nullable=False)
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<TicketStatus %r>' % self.name
