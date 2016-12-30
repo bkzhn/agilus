c9a849481065a6020a77fa2d4e88f1e057d1bdc0
bkzhn <absh.bktl@gmail.com>
2016-12-31 01:43:40 +0600

    add tickets() #api

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,9 +7,21 @@ app.config.from_object('config.Config')
 
 db = SQLAlchemy(app)
 
+
 @app.route('/')
 def index():
     return 'Agilus'
+
+
+@app.route('/tickets')
+def tickets():
+    from models import Ticket
+    tickets = Ticket.query.all()
+
+    return jsonify({
+        'tickets': [i.serialize for i in tickets]
+    })
+
 
 if __name__ == '__main__':
     app.run()
