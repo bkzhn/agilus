19cb98d3a57622aa18a8d2454739d6514bd413e1
bkzhn <absh.bktl@gmail.com>
2017-01-04 02:44:56 +0600

    add test for /ticket : id for exising ticket

--- tests.py
+++ tests.py
@@ -18,5 +18,8 @@ class AgilusTestCase(unittest.TestCase):
         response = self.app.get('/ticket/0')
         assert response.status_code == 404
 
+        response = self.app.get('/ticket/1')
+        assert response.status_code == 200
+
 if __name__ == '__main__':
     unittest.main()
