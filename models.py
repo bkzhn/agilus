88ca532f326c3ada1658996c7f6477e2612fa3d1
bkzhn <absh.bktl@gmail.com>
2017-03-13 23:55:22 +0600

    add docstrings to in models.py #pep8

--- models.py
+++ models.py
@@ -1,41 +1,57 @@
+"""Models."""
+
 from run import db
 from sqlalchemy import ForeignKey
 
 
 class User(db.Model):
+    """User."""
+
     id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
 
     def __init__(self, name):
+        """Constructor."""
         self.name = name
 
     def __repr__(self):
+        """String representation."""
         return '<User %r>' % self.name
 
 
 class TicketType(db.Model):
+    """TicketType."""
+
     id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
 
     def __init__(self, name):
+        """Constructor."""
         self.name = name
 
     def __repr__(self):
+        """String representation."""
         return '<TicketType %r>' % self.name
 
 
 class TicketStatus(db.Model):
+    """TicketStatus."""
+
     id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
 
     def __init__(self, name):
+        """Constructor."""
         self.name = name
 
     def __repr__(self):
+        """String representation."""
         return '<TicketStatus %r>' % self.name
 
 
 class Ticket(db.Model):
+    """Ticket."""
+
     id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
     title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=False)
@@ -45,6 +61,7 @@ class Ticket(db.Model):
     assignee = db.Column(db.Integer, ForeignKey('user.id'), nullable=True)
 
     def __init__(self, title, description, type_id, status_id, author, assignee=None):
+        """Constructor."""
         self.title = title
         self.description = description
         self.type_id = type_id
@@ -53,10 +70,12 @@ class Ticket(db.Model):
         self.assignee = assignee
 
     def __repr__(self):
+        """String representation."""
         return '<Ticket %r>' % self.title
 
     @property
     def serialize(self):
+        """Dictionary representation."""
         return {
             'title': self.title,
             'description': self.description,
