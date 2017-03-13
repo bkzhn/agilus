b8f998ae4cb1e92d35ada96d11077a3740ccfa04
bkzhn <absh.bktl@gmail.com>
2017-03-13 23:31:30 +0600

    move api actions from run.py to api module

--- api/__init__.py
+++ api/__init__.py
@@ -15,3 +15,11 @@ def v1_tickets():
     return jsonify({
         'tickets': [i.serialize for i in tickets]
     })
+
+
+@api.route('/api/v1/ticket/<id>')
+def v1_ticket(id):
+    """Action returns a ticket by id."""
+    from models import Ticket
+    ticket = Ticket.query.get_or_404(id)
+    return jsonify(ticket.serialize)
--- run.py
+++ run.py
@@ -2,8 +2,12 @@ from flask import Flask
 from flask import jsonify
 from flask_sqlalchemy import SQLAlchemy
 
+from api import api
+
 app = Flask(__name__)
 app.config.from_object('config.Config')
+
+app.register_blueprint(api)
 
 db = SQLAlchemy(app)
 
@@ -11,23 +15,6 @@ db = SQLAlchemy(app)
 @app.route('/')
 def index():
     return 'Agilus'
-
-
-@app.route('/tickets')
-def tickets():
-    from models import Ticket
-    tickets = Ticket.query.all()
-
-    return jsonify({
-        'tickets': [i.serialize for i in tickets]
-    })
-
-
-@app.route('/ticket/<id>')
-def ticket(id):
-    from models import Ticket
-    ticket = Ticket.query.get_or_404(id)
-    return jsonify(ticket.serialize)
 
 
 if __name__ == '__main__':
