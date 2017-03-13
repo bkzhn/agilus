7067f1e665dfc5e902b958e00e4f1e1f59b22bb5
bkzhn <absh.bktl@gmail.com>
2017-03-13 23:40:21 +0600

    update and format tests.py #pep8

--- tests.py
+++ tests.py
@@ -1,28 +1,37 @@
+"""Tests."""
+
 import unittest
 
 from run import app
 
+
 class AgilusTestCase(unittest.TestCase):
+    """AgilusTestCase."""
+
     def setUp(self):
+        """Pre-test method."""
         self.app = app.test_client()
 
     def test_homepage(self):
+        """Test for the homepage."""
         response = self.app.get('/')
         assert b'Agilus' in response.data
         assert response.status_code == 200
 
     def test_ticket(self):
-        response = self.app.get('/ticket')
+        """Test for the ticket API."""
+        response = self.app.get('/api/v1/ticket')
         assert response.status_code == 404
 
-        response = self.app.get('/ticket/0')
+        response = self.app.get('/api/v1/ticket/0')
         assert response.status_code == 404
 
-        response = self.app.get('/ticket/1')
+        response = self.app.get('/api/v1/ticket/1')
         assert response.status_code == 200
 
-        response = self.app.post('ticket/1')
+        response = self.app.post('/api/v1/ticket/1')
         assert response.status_code == 405
+
 
 if __name__ == '__main__':
     unittest.main()
