e916394985c805390b3019e421999b742d6b917b
bkzhn <bkzhns@gmail.com>
2023-07-29 00:26:52 -0700

    Feat/Docker (#16)
    * WIP
    
    * fix: docker

--- app/api/routes/tickets.py
+++ app/api/routes/tickets.py
@@ -3,13 +3,13 @@
 from flask import jsonify
 from flask import Blueprint
 
-api = Blueprint('api', __name__)
+tickets_api = Blueprint('api', __name__)
 
 
-@api.route('/api/v1/tickets')
+@tickets_api.route('/api/v1/tickets')
 def v1_tickets():
     """Action returns list of tickets."""
-    from models import Ticket
+    from api.models import Ticket
     tickets = Ticket.query.all()
 
     return jsonify({
@@ -17,9 +17,9 @@ def v1_tickets():
     })
 
 
-@api.route('/api/v1/ticket/<id>')
+@tickets_api.route('/api/v1/ticket/<id>')
 def v1_ticket(id):
     """Action returns a ticket by id."""
-    from models import Ticket
+    from api.models import Ticket
     ticket = Ticket.query.get_or_404(id)
     return jsonify(ticket.serialize)
