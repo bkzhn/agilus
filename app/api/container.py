172ff79f2d4440b40f95ba88f25210673657b951
bkzhn <bkzhns@gmail.com>
2024-09-04 16:18:24 -0700

    Update DI container

--- app/api/container.py
+++ app/api/container.py
@@ -1,10 +1,15 @@
 from api.config import Config
 from api.db import Database
 from api.services.user_service import UserService
+from api.services.ticket_service import TicketService
 
 
 class Container:
     """DI container."""
     db = Database(dsn=Config.SQLALCHEMY_DATABASE_URI)
 
-    user_service = UserService(session=db.session)
+    db_session = db.session()
+
+    user_service = UserService(session=db_session)
+
+    ticket_service = TicketService(session=db_session)
