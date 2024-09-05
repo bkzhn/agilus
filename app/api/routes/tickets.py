576d75d147ee83c6e6c5dd589ca505313e842c7c
bkzhn <bkzhns@gmail.com>
2024-09-05 01:56:46 -0700

    Update tickets.py
    - add v1_create_ticket()
    - add v1_delete_ticket()

--- app/api/routes/tickets.py
+++ app/api/routes/tickets.py
@@ -1,5 +1,4 @@
 """API."""
-from crypt import methods
 
 from flask import (
     request,
@@ -11,7 +10,7 @@ from flask import (
 tickets_api = Blueprint('tickets_api', __name__)
 
 
-@tickets_api.route('/api/v1/tickets')
+@tickets_api.route('/api/v1/tickets', methods=['GET'])
 def v1_tickets():
     """Action returns list of tickets."""
     tickets = app.container.ticket_service.get_tickets()
@@ -20,11 +19,61 @@ def v1_tickets():
     })
 
 
-@tickets_api.route('/api/v1/tickets/<id>')
+@tickets_api.route('/api/v1/tickets', methods=['POST'])
+def v1_create_ticket():
+    """Action creates new ticket."""
+    data = request.json
+
+    title = data.get('title')
+    if not title:
+        raise Exception('Invalid title')
+
+    description = data.get('description')
+    if not description:
+        raise Exception('Invalid description')
+
+    type_id = data.get('type_id')
+    if not type_id:
+        raise Exception('Invalid type id')
+
+    status_id = data.get('status_id')
+    if not status_id:
+        raise Exception('Invalid status id')
+
+    author_id = data.get('author_id')
+    if not author_id:
+        raise Exception('Invalid author id')
+
+    assignee_id = data.get('assignee_id')
+
+    ticket_id = app.container.ticket_service.create_ticket(
+        title=title,
+        description=description,
+        type_id=type_id,
+        status_id=status_id,
+        author_id=author_id,
+        assignee_id=assignee_id,
+    )
+    return jsonify({
+        'ticket_id': ticket_id
+    })
+
+
+@tickets_api.route('/api/v1/tickets/<id>', methods=['GET'])
 def v1_ticket(id):
     """Action returns a ticket by id."""
     ticket = app.container.ticket_service.get_ticket_by_id(id)
     return jsonify(ticket.serialize)
+
+
+@tickets_api.route('/api/v1/tickets/<id>', methods=['DELETE'])
+def v1_delete_ticket(id):
+    """Action deletes ticket by ID."""
+    is_deleted = app.container.ticket_service.delete_ticket_by_id(id)
+    return jsonify({
+        'ticket_id': id,
+        'is_deleted': is_deleted,
+    })
 
 
 @tickets_api.route('/api/v1/ticket-types', methods=['GET'])
@@ -90,6 +139,13 @@ def v1_create_ticket_status():
     })
 
 
+@tickets_api.route('/api/v1/ticket-statuses/<id>', methods=['GET'])
+def v1_ticket_status(id):
+    """Action returns a ticket status by id."""
+    ticket_status = app.container.ticket_service.get_ticket_status_by_id(id)
+    return jsonify(ticket_status.serialize)
+
+
 @tickets_api.route('/api/v1/ticket-statuses/<id>', methods=['DELETE'])
 def v1_delete_ticket_status(id):
     """Action deletes ticket status by ID."""
@@ -98,11 +154,3 @@ def v1_delete_ticket_status(id):
         'ticket_status_id': id,
         'is_deleted': is_deleted,
     })
-
-
-@tickets_api.route('/api/v1/ticket-statuses/<id>')
-def v1_ticket_status(id):
-    """Action returns a ticket status by id."""
-    ticket_status = app.container.ticket_service.get_ticket_status_by_id(id)
-    return jsonify(ticket_status.serialize)
-
