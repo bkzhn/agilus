2888e42e02c47e37048f11054c24c58f4b45d4c2
bkzhn <bkzhns@gmail.com>
2024-09-04 17:21:28 -0700

    Feat/Postgres (#21)
    * Change MySQL to Postgres in docker-compose.yml
    
    * Update requirments.txt
    
    - replace PyMySQL with psycopg2-binary pip package
    
    * Update init.sql
    
    - update MySQL script to Postgres
    
    * Update config.py
    
    - update connection string to Postgres

--- schemas/init.sql
+++ schemas/init.sql
@@ -1,36 +1,26 @@
-create database if not exists agilus;
-
-create or replace table ticket_statuses
-(
-    id   int auto_increment
-        primary key,
+create table if not exists ticket_statuses(
+    id   serial primary key,
     name varchar(100) not null
 );
 
-create or replace table ticket_types
-(
-    id   int auto_increment
-        primary key,
+create table if not exists ticket_types(
+    id   serial primary key,
     name varchar(100) not null
 );
 
-create or replace table users
-(
-    id   int auto_increment
-        primary key,
+create table if not exists users(
+    id   serial primary key,
     name varchar(100) not null
 );
 
-create or replace table tickets
-(
-    id          int auto_increment
-        primary key,
+create table if not exists tickets(
+    id          serial primary key,
     title       varchar(100) not null,
-    description text         not null,
-    type_id     int          not null,
-    status_id   int          not null,
-    author_id   int          not null,
-    assignee_id int          null,
+    description text not null,
+    type_id     int not null,
+    status_id   int not null,
+    author_id   int not null,
+    assignee_id int,
     constraint tickets__ticket_statuses_id_fk
         foreign key (status_id) references ticket_statuses (id),
     constraint tickets__ticket_types_id_fk
@@ -40,4 +30,3 @@ create or replace table tickets
     constraint tickets__users_id_fk2
         foreign key (assignee_id) references users (id)
 );
-
