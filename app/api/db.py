c83d14cd41bf8352d3cea966bbae39085d82a424
bkzhn <bkzhns@gmail.com>
2024-09-05 12:25:42 -0700

    Update db.py
    - update import for declarative_base()

--- app/api/db.py
+++ app/api/db.py
@@ -1,7 +1,11 @@
 """DB"""
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session, Session, sessionmaker
+from sqlalchemy.orm import (
+    declarative_base,
+    Session,
+    sessionmaker,
+    scoped_session,
+)
 
 Base = declarative_base()
 
