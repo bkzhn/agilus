211e9b1dcb9c3f94147c04dbdd675e74c8e7d339
bkzhn <bkzhns@gmail.com>
2024-09-05 21:11:47 -0700

    Move DATABASE_URI to .env file

--- app/api/config.py
+++ app/api/config.py
@@ -1,4 +1,7 @@
+from os import environ
+
+
 class Config(object):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://agilus:agilus@db:5432/agilus'
+    SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
